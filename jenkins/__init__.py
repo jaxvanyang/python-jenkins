5a98031aeed375cd570e487ebfb34edc67f25a5f
Kenyon Ralph <quic_kralph@quicinc.com>
2023-08-03 11:30:10 -0700

    WrappedSession: Fix URL to requests issue 3829
    The repo moved on GitHub and the old URL returns error 404.
    
    Change-Id: Ida67cdfad02accc917fcd4646cc1efaff01bcc5f

--- jenkins/__init__.py
+++ jenkins/__init__.py
@@ -282,7 +282,7 @@ class TimeoutException(JenkinsException):
 class WrappedSession(requests.Session):
     """A wrapper for requests.Session to override 'verify' property, ignoring REQUESTS_CA_BUNDLE environment variable.
 
-    This is a workaround for https://github.com/kennethreitz/requests/issues/3829 (will be fixed in requests 3.0.0)
+    This is a workaround for https://github.com/psf/requests/issues/3829 (will be fixed in requests 3.0.0)
     """
 
     def merge_environment_settings(self, url, proxies, stream, verify, *args,
