jaxvanyang/python-jenkins

commit a8f0d8cfda832c700cf4ca744d07acff3afc5f04

STORY 2010790: Handle 'all' view name when requested 'All'
Jenkins version 2.387.1 or earlier can return 'all' as view name
when requested is 'All'. Add workaround for that.

Change-Id: I6b5327a5f969607ca4838a13bbac838f44e76ccb

jenkins/__init__.py
@@ -1798,6 +1798,8 @@ class Jenkins(object):
             return None
         else:
             actual = json.loads(response)['name']
+            if actual == 'all':
+                actual = 'All'
             if actual != short_name:
                 raise JenkinsException(
                     'Jenkins returned an unexpected view name %s '

commit b147d864468614dd3e2e64304c71847c069f293b

only change view name from all to All when All was requested
Otherwise you get errors like:

    Traceback (most recent call last):
      File "path/bin/jenkins-jobs", line 8, in <module>
        sys.exit(main())
                 ^^^^^^
      File "path/lib64/python3.12/site-packages/jenkins_jobs/cli/entry.py", line 179, in main
        jjb.execute()
      File "path/lib64/python3.12/site-packages/jenkins_jobs/cli/entry.py", line 158, in execute
        ext.obj.execute(self.options, self.jjb_config)
      File "path/lib64/python3.12/site-packages/jenkins_jobs/cli/subcommand/update.py", line 154, in execute
        n = builder.delete_old_managed_views(keep=keep_views)
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "path/lib64/python3.12/site-packages/jenkins_jobs/builder.py", line 402, in delete_old_managed_views
        if view["name"] not in keep and self.is_view(view["name"], use_cache=False):
                                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "path/lib64/python3.12/site-packages/jenkins_jobs/builder.py", line 384, in is_view
        return self.jenkins.view_exists(view_name)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "path/lib64/python3.12/site-packages/jenkins/__init__.py", line 1857, in view_exists
        if self.get_view_name(name) == name:
           ^^^^^^^^^^^^^^^^^^^^^^^^
      File "path/lib64/python3.12/site-packages/jenkins/__init__.py", line 1834, in get_view_name
        raise JenkinsException(
    jenkins.JenkinsException: Jenkins returned an unexpected view name All (expected: all)

Fixes: a8f0d8cfda832c700cf4ca744d07acff3afc5f04
Change-Id: I82b5abd8429190e871e432732d502b0640ebc99b
Signed-off-by: Evgeni Golov <evgeni@golov.de>

jenkins/__init__.py
@@ -1827,7 +1827,7 @@ class Jenkins(object):
             return None
         else:
             actual = json.loads(response)['name']
-            if actual == 'all':
+            if actual == 'all' and short_name == 'All':
                 actual = 'All'
             if actual != short_name:
                 raise JenkinsException(